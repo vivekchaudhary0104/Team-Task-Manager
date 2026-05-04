42a4d736507579d4d1e9b29438dc045a9be83c4b
vivekchaudhary0104 <168198680+vivekchaudhary0104@users.noreply.github.com>
2026-05-05 02:12:04 +0530

    Update CORS origin for production environment

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: 'https://zucchini-embrace-production-1ba1.up.railway.app',
+  origin: 'https://teamstask-manager.up.railway.app',
   credentials: true
 }))
 app.use(express.json());
